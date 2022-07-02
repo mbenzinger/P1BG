f4a20b872fe2ad7d34233f828011100c0a5eae1b
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-02 11:28:13 -0500

    created end game base solution

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let flipErr = document.querySelector("#flipErr")
-    let resetGame = document.querySelector("#resetGame")
-    let errWin = document.querySelector("#err")
-    let youWin = document.querySelector("#you")
-    let total = document.querySelector("#total")
-    let errWinCount = 0
-    let youWinCount = 0
-    let totalCount = 0
-    let errImage = document.querySelector("#errImage")
-    let errMessage = document.querySelector("#errMessage")
-    let ignignoktImage = document.querySelector("#ignignoktImage")
-    let ignignoktMessage = document.querySelector("#ignignoktMessage")
+    let flipErr = document.querySelector("#flipErr");
+    let resetGame = document.querySelector("#resetGame");
+    let errWin = document.querySelector("#err");
+    let youWin = document.querySelector("#you");
+    let total = document.querySelector("#total");
+    let errWinCount = 0;
+    let youWinCount = 0;
+    let totalCount = 0;
+    let errImage = document.querySelector("#errImage");
+    let errMessage = document.querySelector("#errMessage");
+    let ignignoktImage = document.querySelector("#ignignoktImage");
+    let ignignoktMessage = document.querySelector("#ignignoktMessage");
     let errMessages = [
-        'Ya all have any eggs? Cause I\'m totally going to mess someones house up!',
+        'Cause I\'m totally going to mess someones house up!',
         'Yeah, burn it!',
         'We\'re the moon!',
         'You shall do as the Scorpions have done before you!',
@@ -21,9 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
         'On the moon, nerds get their pants pulled down and they are spanked with moon rocks',
         'Take two, mother muchachos!',
         'Hand over those magazines!',
-        'and then you can get tore up!']
+        'and then you can get tore up!'];
     let ignignoktMessages = [
-        'Yes, eggs or pot',
+        'Do you have any eggs?',
         'This place is infinitly boring.',
         'Some would say that the Earth is our moon.',
         'Let us assume that \'Here\' is short for \"Here I am, rock you like a hurricane\".',
@@ -31,15 +31,20 @@ document.addEventListener('DOMContentLoaded', function () {
         'Our race is hundreds of years beyond yours.',
         'Your roommate is a nerd.',
         'I hope he can see this, because I\'m doing it as hard as I possibly can.',
-        'Yes, give us your pornography.',
-        'We shall aquire some wine on the way to the mall.']
+        'Give us your pornography.',
+        'We shall aquire some wine on the way to the mall.'];
+    let gameOver = false;
+    let gameLimit = 7;
     //let sceneImage = [
     //    'assets/images/ATHF_House.jpg',
     //    'assets/images/ATHF_CarlsHouse.png',
     //    'assets/images/ATHF_Mall.jpg']
     //let i=0
 
+
+
     flipErr.addEventListener('click', function() {
+        if (!gameOver) {
         let whoWins = Math.round(Math.random())
         totalCount++
         if (whoWins == 0) {
@@ -64,17 +69,26 @@ document.addEventListener('DOMContentLoaded', function () {
         errWin.textContent = errWinCount;
         youWin.textContent = youWinCount;
         total.textContent = totalCount;
-    })
+        if (totalCount == gameLimit) {gameOver = true}
+    } else {
+        if (youWinCount>errWinCount) {
+            alert("YOU WIN, CONGRADULATIONS!")
+        } else {
+            alert("SORRY, THE MOONINITES WIN!")
+        }
+    }})
 
     resetGame.addEventListener('click', function() {
             errImage.src = "assets/images/err-feet.png";
-            errMessage.textContent = "DAMN NO!";
             ignignoktMessage.textContent = "Is your ego satsified?";
+            errMessage.textContent = "DAMN NO!";
             errWinCount = 0;
             youWinCount = 0;
             totalCount = 0;
             errWin.textContent = errWinCount;
             youWin.textContent = youWinCount;
             total.textContent = totalCount;
+            gameOver = false;
     })
+
 })
