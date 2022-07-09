42c379586f49875e2070b66323653840adb4c4e2
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-09 10:22:35 -0500

    moved global variables out of doc load event

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
+let errMessages = [
+    'Cause I\'m totally going to mess someones house up!',
+    'Yeah, burn it!',
+    'We\'re the moon!',
+    'You shall do as the Scorpions have done before you!',
+    'We smoke as we shoot the bird!',
+    'Man, you hear what he\'s saying?!',
+    'On the moon, nerds get their pants pulled down and they are spanked with moon rocks',
+    'Challenge us!',
+    'Hand over those magazines!',
+    'and then you can get tore up!'];
+let ignignoktMessages = [
+    'Do you have any eggs?',
+    'This place is infinitly boring.',
+    'Some would say that the Earth is our moon.',
+    'Let us assume that \'Here\' is short for \"Here I am, rock you like a hurricane\".',
+    'Light up.',
+    'Our race is hundreds of years beyond yours.',
+    'Your roommate is a nerd.',
+    'Our vertical leap is beyond all measurement.',
+    'Give us your pornography.',
+    'We shall aquire some wine on the way to the mall.'];
+let gameOver = false;
+let gameLimit = 5;
+let sceneImage = [
+    '"url(assets/images/ATHF_House.jpg)"',
+    '"url(assets/images/ATHF_CarlsHouse.png)"',
+    '"url(assets/images/ATHF_Mall.jpg)"']
+let i=0
+let errWinCount = 0;
+let youWinCount = 0;
+let totalCount = 0;
 document.addEventListener('DOMContentLoaded', function () {
     let flipErr = document.querySelector("#flipErr");
     let resetGame = document.querySelector("#resetGame");
     let errWin = document.querySelector("#err");
     let youWin = document.querySelector("#you");
     let total = document.querySelector("#total");
-    let errWinCount = 0;
-    let youWinCount = 0;
-    let totalCount = 0;
     let errImage = document.querySelector("#errImage");
     let errMessage = document.querySelector("#errMessage");
     let ignignoktImage = document.querySelector("#ignignoktImage");
     let ignignoktMessage = document.querySelector("#ignignoktMessage");
-    let errMessages = [
-        'Cause I\'m totally going to mess someones house up!',
-        'Yeah, burn it!',
-        'We\'re the moon!',
-        'You shall do as the Scorpions have done before you!',
-        'We smoke as we shoot the bird!',
-        'Man, you hear what he\'s saying?!',
-        'On the moon, nerds get their pants pulled down and they are spanked with moon rocks',
-        'Challenge us!',
-        'Hand over those magazines!',
-        'and then you can get tore up!'];
-    let ignignoktMessages = [
-        'Do you have any eggs?',
-        'This place is infinitly boring.',
-        'Some would say that the Earth is our moon.',
-        'Let us assume that \'Here\' is short for \"Here I am, rock you like a hurricane\".',
-        'Light up.',
-        'Our race is hundreds of years beyond yours.',
-        'Your roommate is a nerd.',
-        'Our vertical leap is beyond all measurement.',
-        'Give us your pornography.',
-        'We shall aquire some wine on the way to the mall.'];
-    let gameOver = false;
-    let gameLimit = 5;
-    let sceneImage = [
-        '"url(assets/images/ATHF_House.jpg)"',
-        '"url(assets/images/ATHF_CarlsHouse.png)"',
-        '"url(assets/images/ATHF_Mall.jpg)"']
-    let i=0
     document.getElementById('body').style.backgroundImage= 'url(assets/images/ATHF_House.jpg)';
 
     flipErr.addEventListener('click', function() {
